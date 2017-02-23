5d1f36c645912032f7b81989e9e133aa70fdbe7d
kaeld <kaeld@alleninstitute.org>
2017-02-23 14:53:51 -0800

    Forgot to add actual LIF_ASC code.

---
+++ GlifModel/glif_lif_asc.cpp
@@ -0,0 +1,290 @@
+#include "glif_lif_asc.h"
+
+// C++ includes:
+#include <limits>
+#include <iostream>
+
+// Includes from libnestutil:
+#include "numerics.h"
+
+// Includes from nestkernel:
+#include "exceptions.h"
+#include "kernel_manager.h"
+#include "universal_data_logger_impl.h"
+#include "name.h"
+
+// Includes from sli:
+#include "dict.h"
+#include "dictutils.h"
+#include "doubledatum.h"
+#include "integerdatum.h"
+#include "lockptrdatum.h"
+
+
+using namespace nest;
+
+
+nest::RecordablesMap< allen::glif_lif_asc >
+  allen::glif_lif_asc::recordablesMap_;
+
+namespace nest
+{
+// Override the create() method with one call to RecordablesMap::insert_()
+// for each quantity to be recorded.
+template <>
+void
+RecordablesMap< allen::glif_lif_asc >::create()
+{
+  // use standard names whereever you can for consistency!
+  insert_( names::V_m, &allen::glif_lif_asc::get_V_m_ );
+}
+}
+
+/* ----------------------------------------------------------------
+ * Default constructors defining default parameters and state
+ * ---------------------------------------------------------------- */
+
+allen::glif_lif_asc::Parameters_::Parameters_()
+  : V_th_(0.0265) 
+  , G_(4.6951e-09)
+  , E_l_(-0.0774)
+  , C_m_(9.9182e-11)
+  , t_ref_(1.0)
+  , V_reset_(0.0)
+  , asc_init_(std::vector<double>(2, 0.0))
+  , k_(std::vector<double>(2, 0.0))
+  , asc_amps_(std::vector<double>(2, 0.0))
+  , r_(std::vector<double>(2, 1.0))
+{
+}
+
+allen::glif_lif_asc::State_::State_( const Parameters_& p )
+  : V_m_(0.0)
+  , ASCurrents_(std::vector<double>(2, 0.0))
+  , I_(0.0)
+{
+}
+
+/* ----------------------------------------------------------------
+ * Parameter and state extractions and manipulation functions
+ * ---------------------------------------------------------------- */
+
+void
+allen::glif_lif_asc::Parameters_::get( DictionaryDatum& d ) const
+{
+  def<double>(d, names::V_th, V_th_);
+  def<double>(d, names::g, G_);
+  def<double>(d, names::E_L, E_l_);
+  def<double>(d, names::C_m, C_m_);
+  def<double>(d, names::t_ref, t_ref_);
+  def<double>(d, names::V_reset, V_reset_);
+  def< std::vector<double> >(d, Name("asc_init"), asc_init_);
+  def< std::vector<double> >(d, Name("k"), k_ );
+  def< std::vector<double> >(d, Name("asc_amps"), asc_amps_);
+  def< std::vector<double> >(d, Name("r"), r_);
+}
+
+void
+allen::glif_lif_asc::Parameters_::set( const DictionaryDatum& d )
+{
+  updateValue< double >(d, names::V_th, V_th_ );
+  updateValue< double >(d, names::g, G_ );
+  updateValue< double >(d, names::E_L, E_l_ );
+  updateValue< double >(d, names::C_m, C_m_ );
+  updateValue< double >(d, names::t_ref, t_ref_ );
+  updateValue< double >(d, names::V_reset, V_reset_ );
+  updateValue< std::vector<double> >(d, Name("asc_init"), asc_init_);
+  updateValue< std::vector<double> >(d, Name("k"), k_ );
+  updateValue< std::vector<double> >(d, Name("asc_amps"), asc_amps_);
+  updateValue< std::vector<double> >(d, Name("r"), r_);
+}
+
+void
+allen::glif_lif_asc::State_::get( DictionaryDatum& d ) const
+{
+  def< double >(d, names::V_m, V_m_ );
+
+  //( *d )[ names::V_m ] = V_m;
+}
+
+void
+allen::glif_lif_asc::State_::set( const DictionaryDatum& d,
+  const Parameters_& p )
+{
+  // Only the membrane potential can be set; one could also make other state
+  // variables
+  // settable.
+  updateValue< double >( d, names::V_m, V_m_ );
+}
+
+allen::glif_lif_asc::Buffers_::Buffers_( glif_lif_asc& n )
+  : logger_( n )
+{
+}
+
+allen::glif_lif_asc::Buffers_::Buffers_( const Buffers_&, glif_lif_asc& n )
+  : logger_( n )
+{
+}
+
+
+/* ----------------------------------------------------------------
+ * Default and copy constructor for node
+ * ---------------------------------------------------------------- */
+
+allen::glif_lif_asc::glif_lif_asc()
+  : Archiving_Node()
+  , P_()
+  , S_( P_ )
+  , B_( *this )
+{
+  recordablesMap_.create();
+}
+
+allen::glif_lif_asc::glif_lif_asc( const glif_lif_asc& n )
+  : Archiving_Node( n )
+  , P_( n.P_ )
+  , S_( n.S_ )
+  , B_( n.B_, *this )
+{
+}
+
+/* ----------------------------------------------------------------
+ * Node initialization functions
+ * ---------------------------------------------------------------- */
+
+void
+allen::glif_lif_asc::init_state_( const Node& proto )
+{
+  const glif_lif_asc& pr = downcast< glif_lif_asc >( proto );
+  S_ = pr.S_;
+}
+
+void
+allen::glif_lif_asc::init_buffers_()
+{
+  B_.spikes_.clear();   // includes resize
+  B_.currents_.clear(); // include resize
+  B_.logger_.reset();  // includes resize
+}
+
+void
+allen::glif_lif_asc::calibrate()
+{
+  B_.logger_.init();
+
+  V_.t_ref_remaining_ = 0.0;
+  V_.t_ref_total_ = P_.t_ref_ * 1.0e-04;
+
+}
+
+/* ----------------------------------------------------------------
+ * Update and spike handling functions
+ * ---------------------------------------------------------------- */
+
+void
+allen::glif_lif_asc::update( Time const& origin, const long from, const long to )
+{ 
+  const double dt = Time::get_resolution().get_ms() * 1.0e-04;
+
+  double v_old = S_.V_m_;
+  double ASCurrent_old_sum = 0.0;
+
+
+  for ( long lag = from; lag < to; ++lag )
+  {
+
+    if( V_.t_ref_remaining_ > 0.0)
+    {
+      // While neuron is in refractory period count-down in time steps (since dt
+      // may change while in refractory) while holding the voltage at last peak.
+      V_.t_ref_remaining_ -= dt;
+      if( V_.t_ref_remaining_ < 0.0)
+      {
+		// Reset methods
+      	// Reset ASC_currents
+      	std::cout << "------------" << std::endl; 
+      	std::cout << "threshold = " << P_.V_th_ << std::endl;
+      	std::cout << "old_ASCurrent = [" << S_.ASCurrents_[0] << ", " << S_.ASCurrents_[1] << "]" << std::endl;
+      	//std::cout << "r = [" << P_.r_[0] << ", " << P_.r_[1] << "]" << std::endl;
+      	//std::cout << "dt * cut_length = " << V_.t_ref_total_ << std::endl;
+      	std::cout << "resetting ASCurrents to [";
+      	for(std::size_t a = 0; a < S_.ASCurrents_.size(); ++a)
+      	{
+      		S_.ASCurrents_[a] = P_.asc_amps_[a] + 
+      						   S_.ASCurrents_[a] * P_.r_[a] * std::exp(-P_.k_[a] * V_.t_ref_total_);
+      		std::cout << S_.ASCurrents_[a] << ",";
+      	}
+      	std::cout << "]" << std::endl;
+
+      	std::cout << "V_m = " << S_.V_m_ << std::endl;
+
+      	// Voltage should be reset to 
+        S_.V_m_ = P_.V_reset_;
+      }
+      else
+      {
+        S_.V_m_ = v_old;
+      }
+    }
+    else
+    {
+      // Integrate voltage and currents
+
+      // Calculate new ASCurrents value using expoential methods
+      ASCurrent_old_sum = 0.0;
+      for(std::size_t a = 0; a < S_.ASCurrents_.size(); ++a)
+      {
+      	ASCurrent_old_sum += S_.ASCurrents_[a];
+      	S_.ASCurrents_[a] = S_.ASCurrents_[a]*std::exp(-P_.k_[a] * dt);
+      }
+       
+      // Explicit Euler forward (RK1) to find next V_m value
+      S_.V_m_ = v_old + dt*(S_.I_ + ASCurrent_old_sum - P_.G_*(v_old - P_.E_l_))/P_.C_m_;
+      
+      if( S_.V_m_ > P_.V_th_ ) 
+      {
+        V_.t_ref_remaining_ = V_.t_ref_total_;
+        
+        double spike_offset = (1 - (P_.V_th_ - v_old)/(S_.V_m_ - v_old)) * Time::get_resolution().get_ms();
+        set_spiketime( Time::step( origin.get_steps() + lag + 1 ), spike_offset );
+        SpikeEvent se;
+        kernel().event_delivery_manager.send( *this, se, lag );
+      }
+    }
+
+    S_.I_ = B_.currents_.get_value( lag );
+
+    B_.logger_.record_data( origin.get_steps() + lag);
+
+    v_old = S_.V_m_;
+  }
+}
+
+void
+allen::glif_lif_asc::handle( SpikeEvent& e )
+{
+  assert( e.get_delay() > 0 );
+
+  B_.spikes_.add_value(
+    e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
+    e.get_weight() );
+}
+
+void
+allen::glif_lif_asc::handle( CurrentEvent& e )
+{
+  assert( e.get_delay() > 0 );
+
+  B_.currents_.add_value(
+    e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
+    e.get_weight() * e.get_current() );
+}
+
+// Do not move this function as inline to h-file. It depends on
+// universal_data_logger_impl.h being included here.
+void
+allen::glif_lif_asc::handle( DataLoggingRequest& e )
+{
+  B_.logger_.handle( e ); // the logger does this for us
+}
---
+++ GlifModel/glif_lif_asc.h
@@ -0,0 +1,197 @@
+#ifndef GLIF_LIF_ASC_H
+#define GLIF_LIF_ASC_H
+
+#include "archiving_node.h"
+#include "connection.h"
+#include "event.h"
+#include "nest_types.h"
+#include "ring_buffer.h"
+#include "universal_data_logger.h"
+
+#include "dictdatum.h"
+
+namespace allen
+{
+
+class glif_lif_asc : public nest::Archiving_Node
+{
+public:
+
+  glif_lif_asc();
+
+  glif_lif_asc( const glif_lif_asc& );
+
+  using nest::Node::handle;
+  using nest::Node::handles_test_event;
+
+  nest::port send_test_event( nest::Node&, nest::port, nest::synindex, bool );
+
+  void handle( nest::SpikeEvent& );
+  void handle( nest::CurrentEvent& );
+  void handle( nest::DataLoggingRequest& );
+
+  nest::port handles_test_event( nest::SpikeEvent&, nest::port );
+  nest::port handles_test_event( nest::CurrentEvent&, nest::port );
+  nest::port handles_test_event( nest::DataLoggingRequest&, nest::port );
+
+  void get_status( DictionaryDatum& ) const;
+  void set_status( const DictionaryDatum& );
+
+private:
+  //! Reset parameters and state of neuron.
+
+  //! Reset state of neuron.
+  void init_state_( const Node& proto );
+
+  //! Reset internal buffers of neuron.
+  void init_buffers_();
+
+  //! Initialize auxiliary quantities, leave parameters and state untouched.
+  void calibrate();
+
+  //! Take neuron through given time interval
+  void update( nest::Time const&, const long, const long );
+
+  // The next two classes need to be friends to access the State_ class/member
+  friend class nest::RecordablesMap< glif_lif_asc >;
+  friend class nest::UniversalDataLogger< glif_lif_asc >;
+
+
+  struct Parameters_
+  {
+    double V_th_;  // A constant spiking threshold
+    double G_; // membrane conductance
+    double E_l_; // resting potential
+    double C_m_; // capacitance
+    double t_ref_; // refractory time (ms)
+    double V_reset_; // Membrane voltage following spike
+
+    std::vector<double> asc_init_; // initial values of ASCurrents_
+    std::vector<double> k_; // predefined time scale
+    std::vector<double> asc_amps_;
+    std::vector<double> r_;
+
+    Parameters_();
+
+    void get( DictionaryDatum& ) const;
+    void set( const DictionaryDatum& );
+  };
+
+
+  struct State_
+  {
+    double V_m_;  // membrane potential
+    std::vector<double> ASCurrents_; // after-spike currents
+    double I_; // external current
+
+    State_( const Parameters_& );
+
+    void get( DictionaryDatum& ) const;
+    void set( const DictionaryDatum&, const Parameters_& );
+  };
+
+
+  struct Buffers_
+  {
+    Buffers_( glif_lif_asc& );
+    Buffers_( const Buffers_&, glif_lif_asc& );
+
+    nest::RingBuffer spikes_;   //!< Buffer incoming spikes through delay, as sum
+    nest::RingBuffer currents_; //!< Buffer incoming currents through delay,
+
+    //! Logger for all analog data
+    nest::UniversalDataLogger< glif_lif_asc > logger_;
+  };
+
+  struct Variables_
+  {
+    double t_ref_remaining_; // counter during refractory period, seconds
+    double t_ref_total_; // total time of refractory period, seconds
+  };
+
+  double get_V_m_() const
+  {
+    return S_.V_m_;
+  }
+
+  Parameters_ P_; 
+  State_ S_;      
+  Variables_ V_;  
+  Buffers_ B_;    
+
+  // Mapping of recordables names to access functions
+  static nest::RecordablesMap< glif_lif_asc > recordablesMap_;
+};
+
+inline nest::port
+allen::glif_lif_asc::send_test_event( nest::Node& target,
+  nest::port receptor_type,
+  nest::synindex,
+  bool )
+{
+  nest::SpikeEvent e;
+  e.set_sender( *this );
+  return target.handles_test_event( e, receptor_type );
+}
+
+inline nest::port
+allen::glif_lif_asc::handles_test_event( nest::SpikeEvent&,
+  nest::port receptor_type )
+{
+  if ( receptor_type != 0 )
+    throw nest::UnknownReceptorType( receptor_type, get_name() );
+  return 0;
+}
+
+inline nest::port
+allen::glif_lif_asc::handles_test_event( nest::CurrentEvent&,
+  nest::port receptor_type )
+{
+  if ( receptor_type != 0 )
+    throw nest::UnknownReceptorType( receptor_type, get_name() );
+  return 0;
+}
+
+inline nest::port
+allen::glif_lif_asc::handles_test_event( nest::DataLoggingRequest& dlr,
+  nest::port receptor_type )
+{
+  if ( receptor_type != 0 )
+    throw nest::UnknownReceptorType( receptor_type, get_name() );
+
+  return B_.logger_.connect_logging_device( dlr, recordablesMap_ );
+}
+
+inline void
+glif_lif_asc::get_status( DictionaryDatum& d ) const
+{
+  // get our own parameter and state data
+  P_.get( d );
+  S_.get( d );
+
+  // get information managed by parent class
+  Archiving_Node::get_status( d );
+
+  ( *d )[ nest::names::recordables ] = recordablesMap_.get_list();
+}
+
+inline void
+glif_lif_asc::set_status( const DictionaryDatum& d )
+{
+  Parameters_ ptmp = P_; // temporary copy in case of errors
+  ptmp.set( d );         // throws if BadProperty
+  State_ stmp = S_;      // temporary copy in case of errors
+  stmp.set( d, ptmp );   // throws if BadProperty
+
+  Archiving_Node::set_status( d );
+
+  // if we get here, temporaries contain consistent set of properties
+  P_ = ptmp;
+  S_ = stmp;
+}
+
+} // namespace nest
+
+
+
+#endif
